15952369d9e6c7063bf5a20de500c699b0d3bd23
jenakrk <jak6846@psu.edu>
2024-05-05 17:51:49 -0400

    fixed?

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import LandingPage from './Pages/LandingPage';
 import Products from './Pages/Products';
 import ShoppingCart from './Pages/ShoppingCart';
 import CheckOut from './Pages/CheckOut';
-import MyNav from './myNavbar.js';
+import MyNav from './Components/myNavbar.js';
 import items from './items.json';
 import {useState} from'react';
 
 function App() {
   //console.log(window.location.pathname);
-  const [cart,setCart]=useState([]);
+  const [cart,setCart]=useState(sessionStorage.getItem('cart')?JSON.parse(sessionStorage.getItem('cart')):[]);
   let Component;
   switch (window.location.pathname){
     case '/':
+    case '/about':
     case '/landing':
       Component=LandingPage
       break;
@@ -26,10 +27,50 @@ function App() {
       Component=CheckOut
       break;
   }
+  function modifyCart(item, num){
+    let newCart;
+    if (item==="all"){
+      newCart=[];
+    }
+    else{
+      let index=-1;
+      newCart=[...cart];
+      for (let i=0; i < cart.length; i++){
+        //console.log(cart[i].item);
+        if (cart[i].item.id===item.id){
+          index=i;
+          break;
+        }
+      }
+      if (num>0){
+        if (index>-1){
+          newCart[index].quantity+=num;
+        }
+        else{
+          newCart.push({"item":item,"quantity":1});
+        }
+      }
+      if (num<0){
+        if (index>-1){
+          if (newCart[index].quantity+num<=0){
+            newCart.splice(index,1);
+          }
+          else{
+            newCart[index].quantity+=num;
+          }
+        }
+      }
+      //console.log(newCart);
+    }
+    setCart(newCart);
+    sessionStorage.setItem("cart", JSON.stringify(newCart));
+    //console.log("hi");
+    //console.log(sessionStorage.getItem('cart'));
+  }
   return (
     <div className="App">
       <MyNav />
-      <Component items={items} cart={{"inv":cart,"set":setCart}}/>
+      <Component items={items} cart={{"inv":cart,"set":modifyCart}}/>
     </div>
   );
 }
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,15 +1,24 @@
 import '../index.css';
-export default function Item(properties) {
-    let item=properties.item;
+import {Button} from 'react-bootstrap';
+export default function Item(props) {
+    let item=props.item;
     let maxDescLen=80;// item.name.length>12?35:50;
+    let cartIndex=props.cart.inv.map(element=>element.item.id).indexOf(item.id);
     return (
         <div className="item" key={item.id}>
             <div className='itemInner'>
             <img src={item.img} width="100" alt={item.name} />
             <h3>{item.name}</h3>
             <h4>${item.price}</h4>
-            <p>{item.description.length>maxDescLen? item.description.substring(0,maxDescLen)+'...':item.description}</p>
-            <button onClick={() => console.log('stuf!')}>do stuff!</button>
+            <p style={{margin:"5px"}}>{item.description.length>maxDescLen? item.description.substring(0,maxDescLen)+'...':item.description}</p>
+            <p style={{height:"5px"}}>{item.stock} left in stock!</p>
+            <Button 
+                size="sm"
+                disabled={cartIndex>-1?item.stock<=props.cart.inv[cartIndex].quantity:false}
+                onClick={() =>{
+                props.cart.set(props.item,1)}}>
+                    Add to Cart!
+            </Button>
             </div>
         </div>
     )
